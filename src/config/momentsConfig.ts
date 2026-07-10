e752c9cd979beb5571581982665471a63105e27b
fqzlr <fqzlr@outlook.com>
2026-07-11 06:06:40 +0800

    Update src/config/momentsConfig.ts (via Pages CMS)

--- src/config/momentsConfig.ts
+++ src/config/momentsConfig.ts
@@ -36,7 +36,7 @@ export interface MomentsPageConfig {
 
 // 说说封面配置
 export const momentsCover: MomentsCover = {
-	cover_image: "https://q1.qlogo.cn/g?b=qq&nk=20447289&s=640",
+	cover_image: "http://tu.fqzlr.com/file/beijing/1777365393328_bkg__7_.png",
 	cover_avatar: "https://q1.qlogo.cn/g?b=qq&nk=20447289&s=640",
 	cover_name: "番茄主理人",
 	cover_bio: "躬身入局，心为主理，行有尺度，自持本心.",
